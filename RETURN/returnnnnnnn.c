cf800083cc861dc3a66a17755c81235bc617d965
whitespacescape <15368104568@189.cn>
2025-01-06 09:41:20 +0800

    Update returnnnnnnn.c

--- RETURN/returnnnnnnn.c
+++ RETURN/returnnnnnnn.c
@@ -6,7 +6,7 @@ void returnnnnnnn(){
 int rst(){
     return r * sin(www + t11) + s * cos(www - t12);
     return t * sin(www + t11) + t * cos(www - t12);
-    return i * sin(www - t11) + j * cos(www + t12);
+    return r * sin(www - t11) + s * cos(www + t12);
     return t * sin(www - t11) + t * cos(www + t12);
 } 
 }
