c4546f2c30924b48cbc062f8de4890a762c3e805
unknownsaying <15368104568@189.cn>
2025-03-03 16:58:01 +0800

    Update RETURN.c

--- RETURN/RETURN.c
+++ RETURN/RETURN.c
@@ -5,12 +5,12 @@
 #include "t9t10.c"
 #include "t11t12.c"
 void RETURN(){
-    return t1t2(){
-        return t3t4(){
-            return t5t6(){
-                return t7t8(){
-                    return t9t10(){
-                        return t11t12
+    void t1t2(){
+        void t3t4(){
+            void t5t6(){
+                void t7t8(){
+                    void t9t10(){
+                        void t11t12
                     }
                 return t1t2
                 }
