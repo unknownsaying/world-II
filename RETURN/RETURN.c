d2bab6a8a4e1037f3b50a8a4496b48ecf59fc0bd
unknownsaying <15368104568@189.cn>
2025-03-03 16:57:30 +0800

    Update RETURN.c

--- RETURN/RETURN.c
+++ RETURN/RETURN.c
@@ -1,19 +1,24 @@
-#include "returnn.c"
-#include "returnnn.c"
-#include "returnnnn.c"
-#include "returnnnnn.c"
-#include "returnnnnnn.c"
-#include "returnnnnnnn.c"
+#include "t1t2.c"
+#include "t3t4.c"
+#include "t5t6.c"
+#include "t7t8.c"
+#include "t9t10.c"
+#include "t11t12.c"
 void RETURN(){
-    return returnn(){
-        return returnnn(){
-            return returnnnn(){
-                return returnnnnn(){
-                    return returnnnnnn(){
-                        return returnnnnnnn
+    return t1t2(){
+        return t3t4(){
+            return t5t6(){
+                return t7t8(){
+                    return t9t10(){
+                        return t11t12
                     }
+                return t1t2
                 }
+            return t3t4
             }
+        return t5t6   
         }
+    return t7t8    
     }
+return t11t12    
 }
