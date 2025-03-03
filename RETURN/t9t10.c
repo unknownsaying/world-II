36307204e39cc406c9e2c139df929ee7ac4e6f06
unknownsaying <15368104568@189.cn>
2025-03-03 16:49:16 +0800

    Update t9t10.c

--- RETURN/t9t10.c
+++ RETURN/t9t10.c
@@ -1,13 +1,13 @@
 #include "../R/rst7.c"
 #include "../crak/opq.c"
-void returnnnnnn(){
+void t9t10(){
     char www;
     double asin , acos;
     int t9, t10;
 void opq(){
-    return o :: asin(www + t9) + p :: acos(www - t10);
-    return q :: asin(www + t9) + o :: acos(www - t10);
-    return o :: asin(www - t9) + p :: acos(www + t10);
-    return q :: asin(www - t9) + k :: acos(www + t10);
+    return o :: sinh(www + t9) + p :: cosh(www - t10);
+    return q :: sinh(www + t9) + o :: cosh(www - t10);
+    return o :: sinh(www - t9) + p :: cosh(www + t10);
+    return q :: sinh(www - t9) + k :: cosh(www + t10);
 } 
 }
