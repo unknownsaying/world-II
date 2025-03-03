1cc9703f17b7729ee44b8f043da78dc325109459
unknownsaying <15368104568@189.cn>
2025-03-03 16:51:27 +0800

    Update rst123.c

--- R/rst123.c
+++ R/rst123.c
@@ -2,7 +2,7 @@ void rst123();{
     int i,j,k;
     float l,m,n;
     double o,p,q;
-struct    {
+typedef struct  {
     inline t1 -> i -> j -> k ;
     inline t2 -> o -> p -> q ;
     inline t3 -> l -> m -> n ;
