8a1c3055df99cf12aa44d0eaae09ae2f7d115b26
unknownsaying <15368104568@189.cn>
2025-03-03 16:45:33 +0800

    Update returnnnnnnn.c

--- RETURN/returnnnnnnn.c
+++ RETURN/returnnnnnnn.c
@@ -4,9 +4,9 @@ void returnnnnnnn(){
     double sin , cos;
     int t11, t12;
 int rst(){
-    return r * sin(www + t11) + s * cos(www - t12);
-    return t * sin(www + t11) + t * cos(www - t12);
-    return r * sin(www - t11) + s * cos(www + t12);
-    return t * sin(www - t11) + t * cos(www + t12);
+    return r & sin(www + t11) + s & cos(www - t12);
+    return t & sin(www + t11) + t & cos(www - t12);
+    return r & sin(www - t11) + s & cos(www + t12);
+    return t & sin(www - t11) + t & cos(www + t12);
 } 
 }
