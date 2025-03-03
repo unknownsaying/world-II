446a8d1937b7fe3a587fd3c5a74b03cae7675900
unknownsaying <15368104568@189.cn>
2025-03-03 16:44:33 +0800

    Update returnnn.c

--- RETURN/returnnn.c
+++ RETURN/returnnn.c
@@ -5,12 +5,12 @@
 #include "math.h"
 void returnnn(){
     char wx;
-    double asin , acos;
+    double exp , log;
     int t3, t4;
 int efgh(){
-    return e % asin(wx + t3) + f / acos(wx - t4);
-    return g % asin(wx + t3) + h / acos(wx - t4);
-    return e % asin(wx - t3) + f / acos(wx + t4);
-    return g % asin(wx - t3) + h / acos(wx + t4);
+    return e % exp(wx + t3) + f / log(wx - t4);
+    return g % exp(wx + t3) + h / log(wx - t4);
+    return e % exp(wx - t3) + f / log(wx + t4);
+    return g % exp(wx - t3) + h / log(wx + t4);
 } 
 }
