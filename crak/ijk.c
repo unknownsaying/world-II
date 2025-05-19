7a3778272a8094bd1a1c2e7f5a36d245a063090a
unknownsaying <15368104568@189.cn>
2025-05-19 18:12:11 +0800

    Update ijk.c

--- crak/ijk.c
+++ crak/ijk.c
@@ -1,27 +1,28 @@
 #include "math.h"
 void main(){
-    int i, j, k, wx;
+    char w;
+    int i, j, k;
 }
 int t1{
 function t1(){
     int2_t
-    sin(wx + i)
+    sin(w + i)
     int4_t
-    cos(wx - j)
+    cos(w - j)
     int8_t
-    sin(wx + k)
+    sin(w + k)
     int16_t
-    cos(wx - k)
+    cos(w - k)
 }}
 int t2{
 function t2(){
     int16_t
-    sin(wx - i)
+    sin(w - i)
     int8_t
-    cos(wx + j)
+    cos(w + j)
     int4_t
-    sin(wx - k)
+    sin(w - k)
     int2_t
-    cos(wx + k)
+    cos(w + k)
 }
 }
